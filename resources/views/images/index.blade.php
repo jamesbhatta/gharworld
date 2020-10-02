bd56583c4e22146d91756d1d3b73044ce8e35690
santoshkalukunda <santoshkalukunda@gmail.com>
2020-10-02 15:14:19 +0545

    dropzone form

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -14,7 +14,11 @@
         <div class="col-md-12 mx-auto">
             <div class="card z-depth-0 sticky-top">
                 <div class="card-body">
-         
+                    <form action="/file-upload" class="dropzone">
+                        <div class="fallback">
+                          <input name="images" id="images" type="file"  multiple/>
+                        </div>
+                      </form>
                 </div>
             </div>
         </div>
--- resources/views/layouts/partials/styles.blade.php
+++ resources/views/layouts/partials/styles.blade.php
@@ -7,10 +7,8 @@
 <link href="{{ asset('assets/mdb/css/style.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/adminlte/css/adminlte.min.css') }}" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
-<<<<<<< HEAD
+<link href="{{ asset('assets/dropzone/dist/dropzone.css') }}" rel="stylesheet">
 
-=======
->>>>>>> ac3e15cd9d0f249162036046bc71427b299fd753
 @livewireStyles
 @stack('styles')
 <style>
