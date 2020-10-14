7a96ce0bcb74b91147fcf5f72333a07ce062b4e6
santoshkalukunda <santoshkalukunda@gmail.com>
2020-10-14 11:40:56 +0545

    name show in home page and logout

--- resources/views/theme/partials/header.blade.php
+++ resources/views/theme/partials/header.blade.php
@@ -1,25 +1,43 @@
 <!-- Header Section -->
 <header class="header-section">
     <a href="{{url('/')}}" class="site-logo">
-      <h3 class=" text-white">GharWorld</h3>
+        <h3 class=" text-white">GharWorld</h3>
     </a>
     <nav class="header-nav">
-        
+
         <ul class="main-menu">
-        <li><a href="{{url('/')}}" class="active">Home</a></li>
-        <li><a href="{{url('properties/search?type=real-estate&for=all')}}">Real Estate</a></li>
-        <li><a href="{{url('properties/search?type=room&for=rent')}}">Room Rent</a></li>
-        <li><a href="{{route('frontend.professions')}}">Local Contact</a></li> 
-        <li><a href="{{url('page/about')}}">About</a></li>
-        <li><a href="{{url('contact')}}">Contact</a></li>
-    </ul>
+            <li><a href="{{url('/')}}" class="active">Home</a></li>
+            <li><a href="{{url('properties/search?type=real-estate&for=all')}}">Real Estate</a></li>
+            <li><a href="{{url('properties/search?type=room&for=rent')}}">Room Rent</a></li>
+            <li><a href="{{route('frontend.professions')}}">Local Contact</a></li>
+            <li><a href="{{url('page/about')}}">About</a></li>
+            <li><a href="{{url('contact')}}">Contact</a></li>
+        </ul>
         <div class="header-right">
             <div class="user-panel">
+                @if (Route::has('login'))
+                @auth
+                <div class="dropdown">
+                    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ Auth::user()->name }}
+                </a>
+                <div class="dropdown-menu bg-dark" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="{{url('login')}}">Dashbord</a>
+                    <a class="dropdown-item" href="#">Profile</a>
+                    <a class="dropdown-item" href="#"  onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </div>
+            @else
             <a href="{{url('login')}}" class="login">Sign in</a>
             <a href="{{url('register')}}" class="register">Join us</a>
-            </div>
+            @endauth
+            @endif
         </div>
-    
+        </div>
+
     </nav>
 </header>
 <!-- Header Section end -->
--- resources/views/theme/partials/script.blade.php
+++ resources/views/theme/partials/script.blade.php
@@ -1,5 +1,6 @@
 	<!--====== Javascripts & Jquery ======-->
 	<script src="{{asset('assets/mondy/js/jquery-3.2.1.min.js')}}"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="{{asset('assets/mondy/js/bootstrap.min.js')}}"></script>
 	<script src="{{asset('assets/mondy/js/jquery.slicknav.min.js')}}"></script>
 	<script src="{{asset('assets/mondy/js/jquery.magnific-popup.min.js')}}"></script>
