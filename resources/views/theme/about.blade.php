06b99730fd7e4b33cca8fb2ecbe442f186dcd742
santoshkalukunda <santoshkalukunda@gmail.com>
2020-10-18 11:29:16 +0545

    frontend about page

--- resources/views/theme/about.blade.php
+++ resources/views/theme/about.blade.php
@@ -2,13 +2,13 @@
 
 @section('main-content')
 	<!-- Intro Section end -->
-	<section class="about-section spad">
+	<section class="about-section spad my-5">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12 order-lg-1">
 					<div class="about-text text-center">
 						<h3>WHY CHOOSE US?</h3>
-						<p>Gharworld.com brings together property information - location, price, pictures, interactive maps and property features through dynamic website, mobile interface and SMS. We welcome the participation from real estate players - sellers, renters, agents, builders, developers, investors for dissemination of information in trustworthy environment.</p>
+						<p><a href="http://gharworld.com" class="text-secondary">Gharworld.com</a>  brings together property information - location, price, pictures, interactive maps and property features through dynamic website, mobile interface and SMS. We welcome the participation from real estate players - sellers, renters, agents, builders, developers, investors for dissemination of information in trustworthy environment.</p>
 						
 					</div>
 				</div>
