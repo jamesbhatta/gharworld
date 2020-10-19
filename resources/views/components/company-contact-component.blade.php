688438821f5661415be09a8cea6e89687c00ccd4
santoshkalukunda <santoshkalukunda@gmail.com>
2020-10-19 17:34:53 +0545

    bug fixing footer contact

--- resources/views/components/company-contact-component.blade.php
+++ resources/views/components/company-contact-component.blade.php
@@ -5,8 +5,8 @@
         <div><img src="{{asset('storage/'.$profile->logo) }}" alt="GharWorld Logo" style="height:70px;"></div>
         <div class="py-1">{{$profile->name}}</div>
         <div class="py-1 fa fa-map-marker"> {{$profile->address}}</div>
-        <div class="py-1 fa fa-phone"> {{$profile->contact}}</div>
-        <div class="py-1 fa fa-envelope"> {{$profile->email}}</div>
+        <div class="py-1 fa fa-phone"><a href="tel:{{$profile->contact}}"> {{$profile->contact}}</a> </div>
+        <div class="py-1 fa fa-envelope"><a href="mailto:{{$profile->email}}"> {{$profile->email}}</a> </div>
     </h6>
 </div>
     
