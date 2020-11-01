fb2e876a2a2a8ccd53b5ef65c9222b052846d1c0
santoshkalukunda <santoshkalukunda@gmail.com>
2020-11-01 10:26:20 +0545

    map commented

--- resources/views/theme/property-profile.blade.php
+++ resources/views/theme/property-profile.blade.php
@@ -109,14 +109,14 @@
                         });
 
                 </script>
-                @isset($property->location)
+                {{-- @isset($property->location)
                 <h5 class="my-3">Map View</h5>
                 <div class="map-widget">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14376.077865872314!2d-73.879277264103!3d40.757667781624285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1546528920522"
                         style="border:0" allowfullscreen></iframe>
                 </div>
-                @endisset
+                @endisset --}}
             </div>
             
             <div class="col-lg-4 col-md-8 sidebar">
