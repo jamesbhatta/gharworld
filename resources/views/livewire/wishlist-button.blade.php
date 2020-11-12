e0440a2b67a745cdf25b1fc4d8e915144647f77f
santoshkalukunda <santoshkalukunda@gmail.com>
2020-11-12 16:42:50 +0545

    bug fixing wishlist color

--- resources/views/livewire/wishlist-button.blade.php
+++ resources/views/livewire/wishlist-button.blade.php
@@ -1,7 +1,7 @@
-<span wire:click="toggleWishlist" class="change-icon text-danger">
+<span wire:click="toggleWishlist" class="change-icon ">
     @if($hasInWishlist)
-    <i class="fa fa-heart fa-lg"></i>
+    <i class="fa fa-heart text-danger fa-lg"></i>
     @else
-    <i class="fa fa-heart-o fa-lg"></i>
+    <i class="fa fa-heart-o fa-lg  text-white"></i>
     @endif
 </span>
