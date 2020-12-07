91454429a0eb60e0aa87ec6c4356d0b1ed555ab8
santoshkalukunda <santoshkalukunda@gmail.com>
2020-12-07 13:43:26 +0545

    slider bar add

--- resources/views/theme/property-profile.blade.php
+++ resources/views/theme/property-profile.blade.php
@@ -10,122 +10,161 @@
                 <div class="single-property">
                     <div class="sp-image mb-0">
                         <div class="p-img-wrapper">
-                            <img src="{{ $property->image != null ? asset('storage/' . $property->image) : asset('assets/img/real-estate.jpg') }}"
-                                alt="{{ $property->title }}" style="width:100%; height:auto">
-                            {{-- <div class="sp-badge new text-capitalize">{{ $property->for }}</div> --}}
-                        <div class="wishlist">
-                            <livewire:wishlist-button :property="$property" />
-                        </div>
-                        <div class="rating">
-                            @if ($property->for=='rent')
-                            @if ($property->overall_rating !=null)
-                            <span class="bg-success p-2 text-white">{{$property->overall_rating}} <i
-                                    class="fa fa-star-o"></i></span>
-                            @endif
-                            @endif
+                            <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+                                <!--Indicators-->
+                                <ol class="carousel-indicators">
+                                    @for ($i = 0; $i <=$property->images->count(); $i++)
+                                        <li data-target="#carousel-example-1z" data-slide-to="{{$i}}" class="active">
+                                        </li>
+                                        @endfor
+                                </ol>
+                                <!--/.Indicators-->
+                                <!--Slides-->
+                                <div class="carousel-inner" role="listbox">
+                                    <!--First slide-->
+                                    <div class="carousel-item active">
+                                        <img class="d-block w-100"
+                                            src="{{ $property->image != null ? asset('storage/' . $property->image) : asset('assets/img/real-estate.jpg') }}"
+                                            alt="{{ $property->title }}">
+                                    </div>
+                             
+                                    @foreach ($property->images as $image)
+                                    <div class="carousel-item">
+                                       
+                                            <img class="d-block w-100" src="{{ asset('storage/' . $image->link) }}"
+                                                alt="image">
+                            
+                                    </div>
+                                    @endforeach
+                                </div>
+                                <!--/.Slides-->
+                                <!--Controls-->
+                                <a class="carousel-control-prev" href="#carousel-example-1z" role="button"
+                                    data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carousel-example-1z" role="button"
+                                    data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                                <!--/.Controls-->
+                            </div>
+                            <div class="wishlist">
+                                <livewire:wishlist-button :property="$property" />
+                            </div>
+                            <div class="rating">
+                                @if ($property->for=='rent')
+                                @if ($property->overall_rating !=null)
+                                <span class="bg-success p-2 text-white">{{$property->overall_rating}} <i
+                                        class="fa fa-star-o"></i></span>
+                                @endif
+                                @endif
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-lg-7">
-                        {{-- <h4 class="h4-responsive text-capitalize mb-3"><span
+                    <div class="row mt-3">
+                        <div class="col-lg-7">
+                            {{-- <h4 class="h4-responsive text-capitalize mb-3"><span
                                     class="badge badge-warning text-white p-2">{{ $property->type}}</span></h4> --}}
-                        <h5 class="text-uppercase mb-1 mb-md-0">{{$property->title}}</h5>
-                    </div>
-                    <div class="col-lg-5 text-left text-lg-right">
-                        <div class="property-header">
-                            <h5 class="d-inline-block bg-danger text-white p-2">NRs.
-                                {{ number_format($property->price) ."/". ($property->for == 'rent' ? ucfirst($property->price_per) : '-') }}
-                            </h5>
-                            @if ($property->for == 'sale')
-                            <div class="text-secondary">(Negotiable)</div>
-                            @endif
+                            <h5 class="text-uppercase mb-1 mb-md-0">{{$property->title}}</h5>
                         </div>
-                    </div>
-                </div>
-                <div class="bg-light p-3">
-                    <div class="d-flex">
-                        <div>
-                            <div class="my-2">
-                                <span class="mr-2"><i class="fa fa-map-marker"></i></span>
-                                {{ $property->city->name . ', ' . $property->address_line }}
-                            </div>
-                            <div class="my-2">
-                                <span class="mr-2"><i class="fa fa-user"></i></span>
-                                {{ $property->for == 'sale' ? "$property->name" : $profile->name }}
+                        <div class="col-lg-5 text-left text-lg-right">
+                            <div class="property-header">
+                                <h5 class="d-inline-block bg-danger text-white p-2">NRs.
+                                    {{ number_format($property->price) ."/". ($property->for == 'rent' ? ucfirst($property->price_per) : '-') }}
+                                </h5>
+                                @if ($property->for == 'sale')
+                                <div class="text-secondary">(Negotiable)</div>
+                                @endif
                             </div>
                         </div>
-                        <div class="ml-auto text-white">
-                            @if ($property->for == 'sale')
-                            <a class="badge badge-warning px-3" href="tel:{{ $property->contact}}">
-                                <i class=" fa fa-phone p-2"></i>{{ $property->contact}}</a>
-                            @else
-                            <a class="badge badge-warning px-3" href="tel:{{ $profile->contact}}">
-                                <i class=" fa fa-phone p-2"></i>
-                                {{ $profile->contact}}
-                            </a>
-                            @endif
+                    </div>
+                    <div class="bg-light p-3">
+                        <div class="d-flex">
+                            <div>
+                                <div class="my-2">
+                                    <span class="mr-2"><i class="fa fa-map-marker"></i></span>
+                                    {{ $property->city->name . ', ' . $property->address_line }}
+                                </div>
+                                <div class="my-2">
+                                    <span class="mr-2"><i class="fa fa-user"></i></span>
+                                    {{ $property->for == 'sale' ? "$property->name" : $profile->name }}
+                                </div>
+                            </div>
+                            <div class="ml-auto text-white">
+                                @if ($property->for == 'sale')
+                                <a class="badge badge-warning px-3" href="tel:{{ $property->contact}}">
+                                    <i class=" fa fa-phone p-2"></i>{{ $property->contact}}</a>
+                                @else
+                                <a class="badge badge-warning px-3" href="tel:{{ $profile->contact}}">
+                                    <i class=" fa fa-phone p-2"></i>
+                                    {{ $profile->contact}}
+                                </a>
+                                @endif
 
+                            </div>
                         </div>
                     </div>
+                    @isset($property->features)
+                    <h5 class="h5-responsive my-3">Features</h5>
+                    <div class="bg-light p-3">
+                        {!! str_replace(',', '<span class="mx-2">|</span>', $property->features) !!}
+                    </div>
+                    @endisset
+                    @if($property->facilities->count())
+                    <h5 class="h5-responsive my-3">Facilities</h5>
+                    <div class="row">
+                        @foreach ($property->facilities as $facility)
+                        <div class="col-lg-3 col-5 bg-light mx-lg-4 mx-3 mb-3 py-2 px-2">
+                            <span class="text-success mr-2">
+                                <i class="{{ $facility->icon ?? 'fa fa-check-circle' }}"></i>
+                            </span>
+                            {{ $facility->name }}
+                        </div>
+                        @endforeach
+                    </div>
+                    @endif
                 </div>
-                @isset($property->features)
-                <h5 class="h5-responsive my-3">Features</h5>
-                <div class="bg-light p-3">
-                    {!! str_replace(',', '<span class="mx-2">|</span>', $property->features) !!}
+
+
+                @isset($property->description)
+                <div class="property-text">
+                    <h5 class="h5-responsive my-3">Description</h5>
+                    <p>{!! $property->description !!}</p>
                 </div>
                 @endisset
-                @if($property->facilities->count())
-                <h5 class="h5-responsive my-3">Facilities</h5>
-                <div class="row">
-                    @foreach ($property->facilities as $facility)
-                    <div class="col-lg-3 col-5 bg-light mx-lg-4 mx-3 mb-3 py-2 px-2">
-                        <span class="text-success mr-2">
-                            <i class="{{ $facility->icon ?? 'fa fa-check-circle' }}"></i>
-                        </span>
-                        {{ $facility->name }}
-                    </div>
-                    @endforeach
-                </div>
-                @endif
-            </div>
 
-
-            @isset($property->description)
-            <div class="property-text">
-                <h5 class="h5-responsive my-3">Description</h5>
-                <p>{!! $property->description !!}</p>
-            </div>
-            @endisset
-
-            @if($property->images->count())
+                {{-- @if($property->images->count())
             <h5 class="h5-responsive my-3">Pictures</h5>
             <div id="aniimated-thumbnials">
-                {{-- <div class="row"> --}}
+              
                 @foreach ($property->images as $image)
-                {{-- <div class=" col-lg-4 col-6 p-2"> --}}
+              
                 <a href="{{ asset('storage/' . $image->link) }}">
-                    <img class="py-1 px-1 p-lg-2 col-5 col-lg-3" src="{{ asset('storage/' . $image->link) }}" height="155" />
+                <img class="py-1 px-1 p-lg-2 col-5 col-lg-3" src="{{ asset('storage/' . $image->link) }}"
+                    height="155" />
                 </a>
-                {{-- </div> --}}
+
                 @endforeach
-                {{-- </div> --}}
+
             </div>
-            @endif
+            @endif --}}
             <!-- jQuery -->
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
             <!-- A jQuery plugin that adds cross-browser mouse wheel support. (Optional) -->
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js">
             </script>
             <script src="{{ asset('assets/lightgallery/dist/js/lightgallery-all.min.js') }}"></script>
-            <script src="{{ asset('assets/lightgallery/lib/jquery.mousewheel.min.js') }}"></script>
+            <script src="{{ asset('assets/lightgallery/lib/jquery.mousewheel.min.js') }}"></script> --}}
 
-            <script>
+            {{-- <script>
                 $('#aniimated-thumbnials').lightGallery({
                     thumbnail: true
                 });
                 
-            </script>
+            </script> --}}
             {{-- @isset($property->location)
                 <h5 class="my-3">Map View</h5>
                 <div class="map-widget">
@@ -153,7 +192,15 @@
                 <div class="aw-text">
                     <h5 class="text-center py-3 h5-responsive">Related Property</h5>
                     <div class="row justify-content-center">
+                        @php
+                            $propertyId = $property->id;
+                        @endphp
                         @forelse ($properties as $property)
+                            @php
+                            if ($propertyId == $property->id)
+                                continue;
+                            @endphp
+                     
                         <div class="col-6 col-lg-10 text-color m-0">
                             @include('theme.property-card')
                         </div>
